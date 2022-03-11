66592a16b8a1d10777402c82111e6d1155a9c68e
Omar Nasr <danmaklen@gmail.com>
2022-03-11 22:34:29 +0200

    Single Page Application S3 Web Hosting Action (#1)
    * .idea
    
    * minor Program.cs refactor
    
    * Move original cdk.json to Core folder and hide cdk.out
    
    * SinglePageApplicationWebsite
    
    * CDK
    
    * Print CDK Output
    
    * Github Action
    
    * Minor
    
    * Git Ignore
    
    * Git Ignore

---
+++ Deploy/Infra/SPA/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -l
+
+name=$1
+domainName=$2
+certificateArn=$3
+
+outputFile="cdk.out/outputs.json"
+
+echo "Hello World"
+cdk deploy \
+  --app=Infra.SinglePageApplicationWebsite \
+  --require-approval never \
+  --outputs-file $outputFile \
+  -c Name="$name" \
+  -c DomainName="$domainName" \
+  -c CertificateArn="$certificateArn" \
+  --dry-run --profile=personal
+
+
+bucketArn=$(jq ".[].bucket-arn" $outputFile)
+edgeDomain=$(jq ".[].edge-distribute-endpoint" $outputFile)
+
+echo "Reminder: add CNAME record to your domain \`$domainName\` with value \`$edgeDomain\`"
+echo "::setOutputs name=bucketArn::$bucketArn"
